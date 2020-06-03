e4c79651a8154e6115a6f1b7a5c01688651b7230
Felipe Vale <felipevale@gmail.com>
2020-06-03 11:59:21 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Menu">
+      <ul className="menu-level-1">
+        <li>
+          Auto
+          <ul className="menu-level-2">
+            <li>Cars
+              <ul className="menu-level-3">
+                <li>Vehicles</li>
+                <li>Parts</li>
+                <li>Accessories</li>
+              </ul>
+            </li>
+            <li>Motorcycles</li>
+            <li>Trucks</li>
+          </ul>
+        </li>
+        <li>
+            Gadgets
+            <ul className="menu-level-2">
+              <li>Phones</li>
+              <li>Computers</li>
+            </ul>    
+        </li>
+        <li>
+            Item 3
+            <ul className="menu-level-2">
+              <li>l2-Item 1</li>
+              <li>l2-Item 2</li>
+              <li>l2-Item 3</li>
+            </ul> 
+        </li>
+      </ul>
     </div>
   );
 }
